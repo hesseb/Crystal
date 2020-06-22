56cc66086612b67503f2a4b11022a7842bb6317d
hesseb <hesseb@kth.se>
2020-06-23 01:12:35 +0200

    Adds premake-system

---
+++ GenerateProjects.bat
@@ -0,0 +1,2 @@
+call %cd%/vendor/bin/premake/premake5.exe vs2019
+PAUSE
---
+++ premake5.lua
@@ -0,0 +1,109 @@
+workspace "Crystal"
+    architecture "x64"
+
+    startproject "Sandbox"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Crystal"
+    location "Crystal"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        --"%{prj.name}/vendor/"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "CR_PLATFORM_WINDOWS",
+            "CR_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "CR_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "CR_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "CR_DIST"
+        optimize "On"
+
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        --"Crystal/vendor/"
+        "Crystal/src/"
+    }
+
+    links
+    {
+        "Crystal"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "CR_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "CR_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "CR_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "CR_DIST"
+        optimize "On"
+
