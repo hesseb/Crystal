60e69b2219d8e13cf6502e11d6a00edcf21205fb
hesseb <hesseb@kth.se>
2020-06-27 19:20:01 +0200

    Cleans some stuff up, changes gitignore to hopefully include premake bin

--- Crystal/src/Crystal/Log.h
+++ Crystal/src/Crystal/Log.h
@@ -17,23 +17,22 @@ namespace Crystal
 		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
 
 	private:
-#pragma warning( push )
-#pragma warning( disable : 4251 )
-		static std::shared_ptr<spdlog::logger> s_CoreLogger;    //Exporting STL-stuff generates C4251 warning. Isn't a problem here, but should prob be solved at some point.
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
 		static std::shared_ptr<spdlog::logger> s_ClientLogger;
-#pragma warning( pop )
 	};
 }
 
 
-#define CR_CORE_ERROR(...) ::Crystal::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define CR_CORE_WARN(...)  ::Crystal::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define CR_CORE_INFO(...)  ::Crystal::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define CR_CORE_TRACE(...) ::Crystal::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define CR_CORE_CRITICAL(...) ::Crystal::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define CR_CORE_ERROR(...)    ::Crystal::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define CR_CORE_WARN(...)     ::Crystal::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define CR_CORE_INFO(...)     ::Crystal::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define CR_CORE_TRACE(...)    ::Crystal::Log::GetCoreLogger()->trace(__VA_ARGS__)
 
-#define CR_ERROR(...)      ::Crystal::Log::GetClientLogger()->error(__VA_ARGS__)
-#define CR_WARN(...)       ::Crystal::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define CR_INFO(...)       ::Crystal::Log::GetClientLogger()->info(__VA_ARGS__)
-#define CR_TRACE(...)      ::Crystal::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define CR_CRITICAL(...)      ::Crystal::Log::GetClientLogger()->critical(__VA_ARGS__)
+#define CR_ERROR(...)         ::Crystal::Log::GetClientLogger()->error(__VA_ARGS__)
+#define CR_WARN(...)          ::Crystal::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define CR_INFO(...)          ::Crystal::Log::GetClientLogger()->info(__VA_ARGS__)
+#define CR_TRACE(...)         ::Crystal::Log::GetClientLogger()->trace(__VA_ARGS__)
 
 
