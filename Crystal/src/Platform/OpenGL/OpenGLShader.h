cdd167fb987e5236a64115b7120e9caf45523f9e
hesseb <hesseb@kth.se>
2020-06-28 20:34:42 +0200

    Abstracts shaders and vertex buffer layout

--- Crystal/src/Platform/OpenGL/OpenGLShader.h
+++ Crystal/src/Platform/OpenGL/OpenGLShader.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "Crystal/Renderer/Shader.h"
+
+namespace Crystal
+{
+	class OpenGLShader : public Shader
+	{
+	public:
+		OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc);
+		virtual ~OpenGLShader();
+
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+	private:
+		uint32_t m_RendererID;
+	};
+}
